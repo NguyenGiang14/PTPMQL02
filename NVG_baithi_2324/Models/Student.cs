038b94040529f8924d593466c1004f7ce2f4a800
NguyenGiang14 <giangbnbn123@gmail.com>
2024-06-17 13:34:40 +0700

    final  commit

---
+++ NVG_baithi_2324/Models/Student.cs
@@ -0,0 +1,11 @@
+namespace Person.Models
+{
+public class Student
+{
+    public string MaSinhVien { get; set; }
+    public string HoVaTen { get; set; }
+    public string DiaChi { get; set; }
+
+
+}
+}
